f862d6f77c237dd5ab8472c27eea8efd2e618f0f
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2021-08-11 14:49:05 -0400

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,16 @@
+/*
+* Description: A program that prints the numerical value of a given ASCII character
+*/
+
+public class ASCII {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
+© 2021 GitHub, Inc.
